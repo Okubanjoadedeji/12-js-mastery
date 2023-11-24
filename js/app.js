8ba4c0ea2b903abcae56772b1db2d48445b0c0ca
Okubanjoadedeji <okubanjoadedeji39@gmail.com>
2023-11-24 20:45:50 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// //A FUNCTION CALL (calling/executing/invoking a function)
+const result = square(4)
+console.log(result);
+
+// A function declaration
+function name(params) {
+  // Statements
+  // have access to 'this' keyword
+}
